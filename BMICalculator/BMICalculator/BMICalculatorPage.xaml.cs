7276b391357f62f944ed4a3a2d1882f071166f4a
asfend <56428592+codewithasfend@users.noreply.github.com>
2023-05-04 17:49:28 +0500

    BMI Calculator

---
+++ BMICalculator/BMICalculator/BMICalculatorPage.xaml.cs
@@ -0,0 +1,30 @@
+namespace BMICalculator;
+
+public partial class BMICalculatorPage : ContentPage
+{
+    public BMICalculatorPage()
+    {
+        InitializeComponent();
+    }
+
+    private void TapMale_Tapped(object sender, TappedEventArgs e)
+    {
+        FrameMale.BorderColor = Color.FromArgb("#0a0e29");
+        FrameFemale.BorderColor = Color.FromArgb("#fdfdfd");
+    }
+
+    private void TapFemale_Tapped(object sender, TappedEventArgs e)
+    {
+        FrameFemale.BorderColor = Color.FromArgb("#0a0e29");
+        FrameMale.BorderColor = Color.FromArgb("#fdfdfd");
+    }
+
+    private void BtnCalculateBMI_Clicked(object sender, EventArgs e)
+    {
+        double height = double.Parse(LblHeight.Text);
+        double weight = double.Parse(LblWeight.Text);
+
+        double bmi = (weight / height / height) * 10000;
+        Navigation.PushAsync(new BMIResultPage(bmi));
+    }
+}
